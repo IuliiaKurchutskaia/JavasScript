0f12a00a9288d136241e5b33f43182957561e56d
IuliiaKurchitskaia <53300963+IuliiaKurchutskaia@users.noreply.github.com>
2021-02-12 23:29:35 +0300

    add simple js programm

---
+++ arrstyles.js
@@ -0,0 +1,5 @@
+let styles=["ДЖАЗ", "БЛЮЗ"];
+styles.push("Rock");
+styles[styles.length/2]="classica";
+alert(styles.shift);
+styles(styles.unshift("rep","reggi"));
---
+++ randomInt.js
@@ -0,0 +1,7 @@
+function randomInteger(min, max) {
+    // случайное число от min до (max+1)
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+  
+  alert( randomInteger(1, 3) );
